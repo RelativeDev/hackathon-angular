52a4593eb462eccd43661fbad06e9cb30382c895
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2023-02-20 16:43:27 -0200

    ~ domain created

---
+++ src/app/domain/usuario.ts
@@ -0,0 +1,10 @@
+export interface Usuario {
+    id?: any;
+    login: string;
+    nome: string;
+    email: string;
+    senha?: string;
+    dataDeNascimento: any;
+    dataDeCriacao?: any;
+    dataDeAtualizacao?: any;
+}
